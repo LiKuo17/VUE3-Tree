bf089884ce8699ab1474238ad3f5faeaf0dc71cc
JSKUO <1455749306@qq.com>
2023-02-20 13:43:21 +0800

    第四次提交

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="card">
         <header>
-            <div>标题</div>
-            <div>副标题</div>
+            <div>标题1</div>
+            <div>副标题1</div>
         </header>
-        <section>内容</section>
+        <section>内容1</section>
     </div>
 </template>
 
